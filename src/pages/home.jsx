8c182d273964fa7077ae3669ee4a7a452d2b9d8a
FredrikFalto <faltofredrik@gmail.com>
2022-05-23 22:24:27 +0200

    installed axios

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,22 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 import { Button, Container } from 'react-bootstrap';
+import axios from 'axios';
 
-import PremierLeague from './premierleague';
+// import PremierLeague from './premierleague';
+
+const url = 'http://localhost:4000';
+
+const array = [1, 2, 3, 4, 5];
 
 function Home() {
+    // const fetchItems = async () => {
+    //     const data = await fetch('http://localhost:4000/leagues');
+    //     const items = await data.json();
+    //     setItems(items);
+    // };
+
+    // useEffect(() => {
+    //     getLeagues();
+    // }, []);
+
     const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        fetchItems();
-    }, []);
+    const getLeagues = () => {
+        axios
+            .get(url + '/leagues')
+            .then((res) => {
+                console.log(res.data);
+                setItems(res.data);
+                console.log(array);
 
-    const fetchItems = async () => {
-        const data = await fetch('http://localhost:4000/leagues');
-        const items = await data.json();
-        setItems(items);
+                const ligor = items.map(item);
 
-        console.log(items.item.name);
+                console.log(item);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     };
 
     return (
@@ -46,6 +67,20 @@ function Home() {
                     <div className="col-lg-6">
                         <Button variant="outline-danger" className="col-lg-12">
                             La Liga
+                        </Button>
+                    </div>
+                    <div className="col-lg-3"></div>
+                </div>
+
+                <div className="row mt-2">
+                    <div className="col-lg-3"></div>
+                    <div className="col-lg-6">
+                        <Button
+                            variant="danger"
+                            className="col-lg-12"
+                            onClick={getLeagues}
+                        >
+                            Get leagues
                         </Button>
                     </div>
                     <div className="col-lg-3"></div>
