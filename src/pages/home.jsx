906e33bc3b558bb36012bff2f9e256c5a64baeb0
FredrikFalto <faltofredrik@gmail.com>
2022-05-24 11:26:10 +0200

    Fetching leagues from database and creating buttons

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,44 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Axios from 'axios';
 
 import { Button, Container } from 'react-bootstrap';
-import axios from 'axios';
 
 // import PremierLeague from './premierleague';
 
 const url = 'http://localhost:4000';
 
-const array = [1, 2, 3, 4, 5];
-
 function Home() {
-    // const fetchItems = async () => {
-    //     const data = await fetch('http://localhost:4000/leagues');
-    //     const items = await data.json();
-    //     setItems(items);
-    // };
-
-    // useEffect(() => {
-    //     getLeagues();
-    // }, []);
-
     const [items, setItems] = useState([]);
 
     const getLeagues = () => {
-        axios
-            .get(url + '/leagues')
+        Axios.get(url + '/leagues')
             .then((res) => {
-                console.log(res.data);
-                setItems(res.data);
-                console.log(array);
+                const ligor = res.data.map((item) => {
+                    return (
+                        <div key={item._id} className="row mt-2">
+                            <div className="col-lg-3"></div>
+                            <div className="col-lg-6">
+                                <Button variant="danger" className="col-lg-12">
+                                    {item.name}
+                                </Button>
+                            </div>
+                            <div className="col-lg-3"></div>
+                        </div>
+                    );
+                    // return <p key={item._id}>{item.name}</p>;
+                });
 
-                const ligor = items.map(item);
-
-                console.log(item);
+                setItems(ligor);
             })
             .catch((err) => {
                 console.log(err);
             });
     };
+
+    // Calls the getLeagues function to render buttons for every league in the database.
+    getLeagues();
 
     return (
         <div>
@@ -52,39 +50,7 @@ function Home() {
                     </h4>
                 </div>
 
-                <div className="row">
-                    <div className="col-lg-3"></div>
-                    <div className="col-lg-6">
-                        <Button variant="danger" className="col-lg-12">
-                            Premier League
-                        </Button>
-                    </div>
-                    <div className="col-lg-3"></div>
-                </div>
-
-                <div className="row mt-2">
-                    <div className="col-lg-3"></div>
-                    <div className="col-lg-6">
-                        <Button variant="outline-danger" className="col-lg-12">
-                            La Liga
-                        </Button>
-                    </div>
-                    <div className="col-lg-3"></div>
-                </div>
-
-                <div className="row mt-2">
-                    <div className="col-lg-3"></div>
-                    <div className="col-lg-6">
-                        <Button
-                            variant="danger"
-                            className="col-lg-12"
-                            onClick={getLeagues}
-                        >
-                            Get leagues
-                        </Button>
-                    </div>
-                    <div className="col-lg-3"></div>
-                </div>
+                {items}
             </Container>
         </div>
     );
